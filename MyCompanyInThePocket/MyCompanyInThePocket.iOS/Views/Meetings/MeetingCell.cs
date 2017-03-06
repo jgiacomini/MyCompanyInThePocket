b4c3a637a340ad838538f5227f26a58a3aec11ec
jgiacomini <archeone@gmail.com>
2017-03-06 09:25:21 +0100

    Modification du dossier de la MeetingView.

--- MyCompanyInThePocket/MyCompanyInThePocket.iOS/Views/Meetings/MeetingCell.cs
+++ MyCompanyInThePocket/MyCompanyInThePocket.iOS/Views/Meetings/MeetingCell.cs
@@ -8,49 +8,6 @@ using UIKit;
 
 namespace MyCompanyInThePocket.iOS
 {
-	public class MeetingView : UIView
-	{
-		public MeetingView()
-		{
-			Title = new UILabel();
-			DateFormated = new UILabel();
-			Type = new UILabel();
-
-			AddSubviews(Title, DateFormated, Type);
-
-			this.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
-			nfloat margin = 5;
-			this.AddConstraints(
-				Title.AtTopOf(this, margin),
-				Title.AtLeftOf(this, margin),
-				Title.AtRightOf(this, margin),
-				DateFormated.AtBottomOf(this, margin),
-				DateFormated.AtLeftOf(this, margin),
-				Type.AtBottomOf(this, margin),
-				Type.AtRightOf(this, margin));	
-		}
-
-		public UILabel Title
-		{
-			get;
-			set;
-		}
-
-		public UILabel DateFormated
-		{
-			get;
-			set;
-		}
-
-		public UILabel Type
-		{
-			get;
-			set;
-		}
-
-	
-	}
-
 	public class MeetingCell : MvxTableViewCell
 	{
 		public static readonly NSString Key = new NSString("MeetingCell");
---
+++ MyCompanyInThePocket/MyCompanyInThePocket.iOS/Views/Meetings/MeetingView.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Foundation;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Binding.iOS.Views;
+using MyCompanyInThePocket.Core.ViewModels;
+using Cirrious.FluentLayouts.Touch;
+using UIKit;
+
+namespace MyCompanyInThePocket.iOS
+{
+	public class MeetingView : UIView
+	{
+		public MeetingView()
+		{
+			Title = new UILabel();
+			DateFormated = new UILabel();
+			Type = new UILabel();
+
+			AddSubviews(Title, DateFormated, Type);
+
+			this.SubviewsDoNotTranslateAutoresizingMaskIntoConstraints();
+			nfloat margin = 5;
+			this.AddConstraints(
+				Title.AtTopOf(this, margin),
+				Title.AtLeftOf(this, margin),
+				Title.AtRightOf(this, margin),
+				DateFormated.AtBottomOf(this, margin),
+				DateFormated.AtLeftOf(this, margin),
+				Type.AtBottomOf(this, margin),
+				Type.AtRightOf(this, margin));
+		}
+
+		public UILabel Title
+		{
+			get;
+			set;
+		}
+
+		public UILabel DateFormated
+		{
+			get;
+			set;
+		}
+
+		public UILabel Type
+		{
+			get;
+			set;
+		}
+	}
+}
