600bd8d684b78e10fd60bdb3fabe0703607f62e9
JAN <antoine_jonathan@hotmail.com>
2017-04-20 16:42:29 +0200

    integration calendrier natif iOS

--- MyCompanyInThePocket/MyCompanyInThePocket.iOS/Services/iOSNativeCalendarIntegrationService.cs
+++ MyCompanyInThePocket/MyCompanyInThePocket.iOS/Services/iOSNativeCalendarIntegrationService.cs
@@ -1,16 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 using Foundation;
-using UIKit;
 using MyCompanyInThePocket.Core.Services.Interface;
 using MyCompanyInThePocket.Core.Models;
 using System.Threading.Tasks;
 using EventKit;
 using Plugin.Settings;
 using MvvmCross.Platform.iOS;
+using Nito.AsyncEx;
 
 namespace MyCompanyInThePocket.iOS.Services
 {
@@ -27,115 +26,119 @@ namespace MyCompanyInThePocket.iOS.Services
         public iOSNativeCalendarIntegrationService()
         {
             _eventStore = new EKEventStore();
-            //_eventStore.
         }
+
+        private AsyncLock _PushMeetingsToCalendarAsyncLock = new AsyncLock();
 
         public async Task PushMeetingsToCalendarAsync(List<Meeting> meetings)
         {
             try
             {
-                var tcs = new TaskCompletionSource<bool>();
-
-                _eventStore.RequestAccess(EKEntityType.Event, (a, e) => { tcs.TrySetResult(a); });
-
-                // pas de résultat
-                if (!await tcs.Task)
-                {
-                    return;
-                }
-
-                var calendar = _eventStore.GetCalendar(AcraCalendarIdentifier);
-                NSError errorToDelete;
-                _eventStore.RemoveCalendar(calendar, true, out errorToDelete);
-
-                calendar = null;
-                if (calendar == null)
+                using (await _PushMeetingsToCalendarAsyncLock.LockAsync())
                 {
 
-                    calendar = EKCalendar.FromEventStore(_eventStore);
-                    calendar.Title = "ACRA";
+                    var tcs = new TaskCompletionSource<bool>();
 
-                    calendar.Source = _eventStore.Sources.FirstOrDefault(s => s.SourceType == EKSourceType.Subscribed || s.SourceType == EKSourceType.Local);
-                    NSError error;
-                    _eventStore.SaveCalendar(calendar, true, out error);
-                    AcraCalendarIdentifier = calendar.CalendarIdentifier;
-                }
+                    _eventStore.RequestAccess(EKEntityType.Event, (a, e) => { tcs.TrySetResult(a); });
 
-
-                var toSaves = meetings.OrderByDescending(ap => ap.StartDate).ToArray();
-
-                var howMuch = toSaves.Length;
-                for (int index = 0; index < howMuch; index++)
-                {
-                    var appointData = toSaves[index];
-
-                    // If set to AllDay and given an EndDate of 12am the next day, EventKit
-                    // assumes that the event consumes two full days.
-                    // (whereas WinPhone/Android consider that one day, and thus so do we)
-                    //
-                    if (appointData.AllDayEvent)
+                    // pas de résultat
+                    if (!await tcs.Task)
                     {
-                        appointData.EndDate = appointData.EndDate.AddMilliseconds(-1);
+                        return;
                     }
 
-                    var toSave = EKEvent.FromStore(_eventStore);
-
-                    //if (!string.IsNullOrEmpty(appointData.))
-                    //{
-                    //    toSave.Location = appointData.Location;
-                    //}
-                    toSave.StartDate = ConvertDateTimeToNSDate(appointData.StartDate);
-                    toSave.AllDay = appointData.AllDayEvent;
-
-                    if (appointData.IsRecurrent && appointData.AllDayEvent)
+                    var calendar = _eventStore.GetCalendar(AcraCalendarIdentifier);
+                    if (calendar != null)
                     {
-                        var end = EKRecurrenceEnd.FromEndDate(ConvertDateTimeToNSDate(appointData.EndDate));
-                        var rule = new EKRecurrenceRule(EKRecurrenceFrequency.Weekly, 1, end);
-                        toSave.RecurrenceRules = new[] { rule };
+                        NSError errorToDelete;
+                        _eventStore.RemoveCalendar(calendar, true, out errorToDelete);
                     }
-                    else
+
+                    calendar = null;
+                    if (calendar == null)
                     {
-                        toSave.EndDate = ConvertDateTimeToNSDate(appointData.EndDate);
+                        calendar = EKCalendar.FromEventStore(_eventStore);
+                        calendar.Title = "ACRA du " + DateTime.Now.ToString("g");
+
+                        calendar.Source = _eventStore.Sources.FirstOrDefault(s => s.SourceType == EKSourceType.Subscribed || s.SourceType == EKSourceType.Local);
+                        NSError error;
+                        _eventStore.SaveCalendar(calendar, true, out error);
+                        AcraCalendarIdentifier = calendar.CalendarIdentifier;
                     }
 
 
-                    if (appointData.Title != null && appointData.Title.IndexOf("férié", StringComparison.OrdinalIgnoreCase) == -1)
+                    var toSaves = meetings.OrderByDescending(ap => ap.StartDate).ToArray();
+
+                    var howMuch = toSaves.Length;
+                    for (int index = 0; index < howMuch; index++)
                     {
-                        if (appointData.Duration.TotalDays > 1 && !appointData.IsRecurrent)
+                        var appointData = toSaves[index];
+
+
+
+                        var toSave = EKEvent.FromStore(_eventStore);
+
+                        //if (!string.IsNullOrEmpty(appointData.))
+                        //{
+                        //    toSave.Location = appointData.Location;
+                        //}
+                        toSave.StartDate = ConvertDateTimeToNSDate(appointData.StartDate);
+                        toSave.AllDay = appointData.AllDayEvent;
+
+                        if (appointData.IsRecurrent && appointData.AllDayEvent)
                         {
-                            toSave.Title = ((int)appointData.Duration.TotalDays + 1) + " " + appointData.Title;
+                            var end = EKRecurrenceEnd.FromEndDate(ConvertDateTimeToNSDate(appointData.EndDate));
+                            var rule = new EKRecurrenceRule(EKRecurrenceFrequency.Weekly, 1, end);
+                            toSave.RecurrenceRules = new[] { rule };
                         }
                         else
                         {
-                            toSave.Title = appointData.Title;
+                            toSave.EndDate = ConvertDateTimeToNSDate(appointData.EndDate);
                         }
+
+                        // If set to AllDay and given an EndDate of 12am the next day, EventKit
+                        // assumes that the event consumes two full days.
+                        // (whereas WinPhone/Android consider that one day, and thus so do we)
+                        //
+                        if (appointData.AllDayEvent)
+                        {
+                            toSave.EndDate = ConvertDateTimeToNSDate(appointData.EndDate.AddDays(-1));
+                        }
+
+                        if (appointData.Title != null && appointData.Title.IndexOf("férié", StringComparison.OrdinalIgnoreCase) == -1)
+                        {
+                            if (appointData.Duration.TotalDays > 1 && !appointData.IsRecurrent)
+                            {
+                                toSave.Title = ((int)appointData.Duration.TotalDays + 1) + " " + appointData.Title;
+                            }
+                            else
+                            {
+                                toSave.Title = appointData.Title;
+                            }
+                        }
+                        else
+                        {
+                            toSave.Title = "[FERIE] " + appointData.Title;
+                        }
+
+                        toSave.Notes = appointData.Type.ToString();
+
+                        toSave.Calendar = calendar;
+
+                        NSError errorEvent;
+                        _eventStore.SaveEvent(toSave, EKSpan.ThisEvent, out errorEvent);
                     }
-                    else
-                    {
-                        toSave.Title = "[FERIE] " + appointData.Title;
-                    }
-
-                    toSave.Notes = appointData.Type.ToString();
-
-                    toSave.Calendar = calendar;
-
-                    NSError errorEvent;
-                    _eventStore.SaveEvent(toSave, EKSpan.ThisEvent, out errorEvent);
                 }
             }
             catch (Exception)
             {
-
             }
         }
 
         public NSDate ConvertDateTimeToNSDate(DateTime date)
         {
+            date = DateTime.SpecifyKind(date, DateTimeKind.Utc);
             return date.ToNSDate();
-
-            date = date.ToLocalTime();
-            var newDate = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(2001, 1, 1, 0, 0, 0));
-            return NSDate.FromTimeIntervalSinceReferenceDate((date - newDate).TotalSeconds);
         }
     }
 }
